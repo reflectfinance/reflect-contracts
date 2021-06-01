f386ce49003db0711a9a8f1216115c6a0b60af83
Feiyang Tan <37208480+FeiyangTan@users.noreply.github.com>
2021-06-01 15:48:07 +0800

    Update REFLECT.sol
    unnecessary else if

--- contracts/REFLECT.sol
+++ contracts/REFLECT.sol
@@ -158,8 +158,6 @@ contract REFLECT is Context, IERC20, Ownable {
             _transferFromExcluded(sender, recipient, amount);
         } else if (!_isExcluded[sender] && _isExcluded[recipient]) {
             _transferToExcluded(sender, recipient, amount);
-        } else if (!_isExcluded[sender] && !_isExcluded[recipient]) {
-            _transferStandard(sender, recipient, amount);
         } else if (_isExcluded[sender] && _isExcluded[recipient]) {
             _transferBothExcluded(sender, recipient, amount);
         } else {
